ef873f4c050bf290b8a4a2d536bd55146cce28f3
anandajuliar <anandajulia001@gmail.com>
2024-04-04 19:08:42 +0700

    if and while

--- AlgorithmaSearch_0046/AlgorithmaSearch_0046.cpp
+++ AlgorithmaSearch_0046/AlgorithmaSearch_0046.cpp
@@ -52,4 +52,13 @@ void LinearSearch()
             }
             i++; //step 4
         }
+        if (i == n) //step 5
+            cout << "\n" << item << " not found in the array\n";
+        cout << "\nNumber of comparions: " << ctr << endl;
+
+        cout << "\nContinue search (y/n): ";
+        cin >> ch;
+
+    } while ((ch == 'y') || (ch == 'Y'));
+}
 
